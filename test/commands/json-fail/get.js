62c71055e0a0611b4651d09990d92f6ee122ddd3
Juan José <juanjo.herrero@upplication.com>
2016-08-29 17:28:20 +0200

    Add tests to cover some untested lines

---
+++ test/commands/json-fail/get.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+console.log(JSON.stringify({
+	success: false,
+	result: {
+		foo: 'bar'		
+	}
+}))
--- test/test.js
+++ test/test.js
@@ -6,43 +6,99 @@ const dafuq = require(process.env['DAFUQ_COVERAGE'] ? '../src-cov' : '../src')
 
 describe('Constructor', function() {
 
+    const build = function(opts) {
+        return function() {
+            dafuq(opts)
+        }
+    }
+
     it('should throw if no path is provided',  function() {
-        (() => dafuq()).should.throw(/path/);
+        build().should.throw(/path/);
     })
 
     it('should throw if path is not a valid string',  function() {
-        (() => dafuq({ path: ''})).should.throw(/path/);
-        (() => dafuq('')).should.throw(/path/);
-        (() => dafuq({ path: undefined })).should.throw(/path/);
-        (() => dafuq(undefined)).should.throw(/path/);
-        (() => dafuq({ path: null })).should.throw(/path/);
-        (() => dafuq(null)).should.throw(/path/);
-        (() => dafuq({ path: 1 })).should.throw(/path/);
-        (() => dafuq(1)).should.throw(/path/);
-        (() => dafuq({ path: {} })).should.throw(/path/);
-        (() => dafuq({})).should.throw(/path/);
-        (() => dafuq({ path: [] })).should.throw(/path/);
-        (() => dafuq([])).should.throw(/path/);
+        build({ path: ''}).should.throw(/path/);
+        build('').should.throw(/path/);
+        build({ path: undefined }).should.throw(/path/);
+        build(undefined).should.throw(/path/);
+        build({ path: null }).should.throw(/path/);
+        build(null).should.throw(/path/);
+        build({ path: 1 }).should.throw(/path/);
+        build(1).should.throw(/path/);
+        build({ path: {} }).should.throw(/path/);
+        build({}).should.throw(/path/);
+        build({ path: [] }).should.throw(/path/);
+        build([]).should.throw(/path/);
     })
 
     it('should throw if path points to a non existant directory',  function() {
-        (() => dafuq({ path: '/dev/null' })).should.throw(/path/);
-        (() => dafuq('/dev/null')).should.throw(/path/);
-        (() => dafuq({ path: './doesnotexist' })).should.throw(/path/);
-        (() => dafuq('./doesnotexist')).should.throw(/path/);
+        build({ path: '/dev/null' }).should.throw(/path/);
+        build('/dev/null').should.throw(/path/);
+        build({ path: './doesnotexist' }).should.throw(/path/);
+        build('./doesnotexist').should.throw(/path/);
     })
 
     it('should throw if path points to a file',  function() {
-        (() => dafuq({ path: __filename })).should.throw(/path/);
+        build({ path: __filename }).should.throw(/path/);
     })
 
     it('should work with a valid relative directory', () => {
-        (() => dafuq({ path: '../test/commands' })).should.not.throw();
-        (() => dafuq({ path: './commands' })).should.not.throw();
+        build({ path: '../test/commands' }).should.not.throw();
+        build({ path: './commands' }).should.not.throw();
     })
 
     it('should work with a valid absolute directory', () => {
-        (() => dafuq({ path: p.resolve(__dirname, './commands') })).should.not.throw();
+        build({ path: p.resolve(__dirname, './commands') }).should.not.throw();
+    })
+
+    it('should throw if shebang is not a valid string',  function() {
+        build({
+            path: './commands',
+            shebang: 1
+        }).should.throw(/shebang/);
+        build({
+            path: './commands',
+            shebang: {}
+        }).should.throw(/shebang/);
+        build({
+            path: './commands',
+            shebang: []
+        }).should.throw(/shebang/);
+    })
+
+    it('should throw if debug is not a boolean nor a function',  function() {
+        build({
+            path: './commands',
+            debug: false
+        }).should.not.throw();
+        build({
+            path: './commands',
+            debug: true
+        }).should.not.throw();
+        build({
+            path: './commands',
+            debug: function() {}
+        }).should.not.throw();
+        build({
+            path: './commands',
+            debug: ''
+        }).should.throw();
+        build({
+            path: './commands',
+            debug: 'string'
+        }).should.throw();
+        build({
+            path: './commands',
+            debug: 123
+        }).should.throw();
+        build({
+            path: './commands',
+            debug: {}
+        }).should.throw();
+        build({
+            path: './commands',
+            debug: []
+        }).should.throw();
     })
 })
 
@@ -89,6 +145,26 @@ describe('Invoking a file', () => {
                 .end(done)
         })
 
+
+        it('should return the parsed output of executing a command that outputs JSON', (done) => {
+            request(app)
+                .get('/json')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .expect(res => res.body.result.should.be.eql({ 'foo': 'bar' }))
+                .end(done)
+        })
+
+        it('should return the parsed output of executing a command that outputs JSON and has success', (done) => {
+            request(app)
+                .get('/json-fail')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .expect(res => { res.body.success.should.be.false() })
+                .expect(res => res.body.result.should.be.eql({ 'foo': 'bar' }))
+                .end(done)
+        })
+
         it('should return a download file when file exits with code 10', (done) => {
             request(app)
                 .get('/download')
@@ -125,60 +201,6 @@ describe('Invoking a file', () => {
                 path: './commands',
                 shebang: p.join(__dirname, 'node')
             });
-        })
-
-        it('should return success true after executing the command', (done) => {
-            request(app)
-                .get('/hello')
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .expect(res => res.body.success.should.be.true() )
-                .end(done)
-        })
-
-        it('should return the output of executing the command', (done) => {
-            request(app)
-                .get('/hello')
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .expect(res => res.body.result.should.be.equal('Hello World'))
-                .end(done)
-        })
-
-        it('should return the output of executing the command when not specifing extension', (done) => {
-            request(app)
-                .get('/hello')
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .expect(res => res.body.result.should.be.equal('Hello World'))
-                .end(done)
-        })
-
-        it('should return success false if command exits with code different from 0', (done) => {
-            request(app)
-                .get('/bye')
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .expect(res => res.body.success.should.be.false())
-                .end(done)
-        })
-
-        it('should return the output of executing the command even when it exits with code different from 0', (done) => {
-            request(app)
-                .get('/bye')
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .expect(res => res.body.result.should.be.equal('Bye World'))
-                .end(done)
-        })
-
-        it('should return a download file when file exits with code 10', (done) => {
-            request(app)
-                .get('/download')
-                .expect(200)
-                .expect('Content-Disposition', /attachment/)
-                .expect(res => res.text.should.not.be.empty())
-                .end(done)
         })
 
         it('should create a route even when a file doesn\'t have execute permissions', (done) => {
